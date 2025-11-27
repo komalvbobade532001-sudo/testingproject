cfd64c81423ba89447ee9805ba47c856da01a555
komalvbobade532001-sudo <komalvbobade532001@gmail.com>
2025-11-27 23:23:51 +0530

    Komal+

--- second.java
+++ second.java
@@ -2,6 +2,10 @@ package testingproject;
 
 public class second {
     public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("komal -" + i);
+            System.out.println("Iteration: " + i);
+        }
         System.out.println("This is the second Java file.");
     }
 }
